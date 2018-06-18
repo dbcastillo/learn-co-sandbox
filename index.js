f68a55e4e8d3f2b76a12feaafb3932bf3e7e5e1d
derickcastillo38 <9derick@gmail.com>
2018-06-18 18:26:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var name = "Susan";
+var name1 = "Susan";
 var height = 70;
-if (name === "Susan") {
+if (name1 === "Susan") {
   console.log("The name is correct");
 } else {
   console.log("Expected the name to be Susan");
