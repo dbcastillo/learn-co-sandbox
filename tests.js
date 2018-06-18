d47f1d7f546bef9ca124345c425100df871e894f
derickcastillo38 <9derick@gmail.com>
2018-06-18 18:35:38 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 var index = require("./index.js");
 
-if (name === "Susan") {
+if (index.name === "Susan") {
   console.log("The name is correct");
-} else if (height === 70) {
+} else if (index.height === 70) {
   console.log("The height is correct");
 }
