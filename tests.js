4a9601aa1ce995f0b43fe857113b180e7abeaeaf
derickcastillo38 <9derick@gmail.com>
2018-06-18 18:42:11 +0000

    Done.

---
+++ tests.js
@@ -0,0 +1,7 @@
+var index = require("./index.js");
+
+if (index.name === "Susan") {
+  console.log("The name is correct");
+} else if (index.height === 70); {
+  console.log("The height is correct");
+}
